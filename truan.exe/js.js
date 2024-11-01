93ab7a93d235f4a20e0efc4f279ba5fd35a3a740
EXBH <yasimko2011@gmail.com>
2024-11-01 21:55:40 +0600

    Add files via upload

---
+++ truan.exe/js.js
@@ -0,0 +1,192 @@
+let claviotyra = document.getElementById("claviotyra")
+// let q = document.getElementById("q")
+// let Q = document.getElementById("Q")
+document.getElementById("Q").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "Q"
+})
+// ________________________________________
+document.getElementById("W").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "W"
+})
+// ________________________________________
+document.getElementById("E").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "E"
+})
+// ________________________________________
+document.getElementById("R").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "R"
+})
+// ________________________________________
+document.getElementById("T").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "T"
+})
+// ________________________________________
+document.getElementById("Y").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "Y"
+})
+// ________________________________________
+document.getElementById("U").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "U"
+})
+// ________________________________________
+document.getElementById("I").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "I"
+})
+// ________________________________________
+document.getElementById("O").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "O"
+})
+// ________________________________________
+document.getElementById("P").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "P"
+})
+// ________________________________________
+document.getElementById("1").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "1"
+})
+// ________________________________________
+document.getElementById("2").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "2"
+})
+// ________________________________________
+document.getElementById("3").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "3"
+})
+// ________________________________________
+document.getElementById("4").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "4"
+})
+// ________________________________________
+document.getElementById("5").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "5"
+})
+// ________________________________________
+document.getElementById("6").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "6"
+})
+// ________________________________________
+document.getElementById("7").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "7"
+})
+// ________________________________________
+document.getElementById("8").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "8"
+})
+// ________________________________________
+document.getElementById("9").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "9"
+})
+// ________________________________________
+document.getElementById("0").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "0"
+})
+// ________________________________________
+document.getElementById("-").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "-"
+})
+// ________________________________________
+document.getElementById("+").addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "+"
+})
+// ________________________________________
+document.getElementById('"').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += '"'
+})
+// ________________________________________
+document.getElementById('button-mini').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += '~'
+})
+// ________________________________________
+document.getElementById('"').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += '"'
+})
+// ________________________________________
+document.getElementById('skob1').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += '{'
+})
+// ________________________________________
+document.getElementById('skob2').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += '}'
+})
+// ________________________________________
+document.getElementById('A').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'A'
+})
+// ________________________________________
+document.getElementById('S').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'S'
+})
+// ________________________________________
+document.getElementById('D').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'D'
+})
+// ________________________________________
+document.getElementById('F').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'F'
+})
+// ________________________________________
+document.getElementById('G').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'G'
+})
+// ________________________________________
+document.getElementById('H').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'H'
+})
+// ________________________________________
+document.getElementById('J').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += "J"
+})
+// ________________________________________
+document.getElementById('K').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'K'
+})
+// ________________________________________
+document.getElementById('L').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'L'
+})
+// ________________________________________
+document.getElementById('point').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += '.'
+})
+// ________________________________________
+document.getElementById('rovno').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += '='
+})
+// ________________________________________
+document.getElementById('Z').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'Z'
+})
+// ________________________________________
+
+document.getElementById('X').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'X'
+})
+// ________________________________________
+document.getElementById('C').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'C'
+})
+// ________________________________________
+document.getElementById('V').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'V'
+})
+// ________________________________________
+document.getElementById('B').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'B'
+})
+// ________________________________________
+document.getElementById('N').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'N'
+})
+// ________________________________________
+document.getElementById('M').addEventListener("click", function() {
+    document.getElementById("tx1").innerText += 'M'
+})
+// ________________________________________
+function addSpace() {
+    let tx1 = document.getElementById("tx1");
+    tx1.innerHTML += "&nbsp;"
+}
+function backspace() {
+    tx1.textContent = tx1.textContent.slice(0, -1);
+}
+
